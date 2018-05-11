353ff3a865f50bf3702a19b6a212e58792394f8c
Jack Gang <jack.gang@xrtrading.com>
2018-05-11 14:06:57 -0500

    complete version 1 with comments

--- hw5/ImageEffects.hs
+++ hw5/ImageEffects.hs
@@ -1,6 +1,6 @@
 {- 
   File      :  ImageEffects.hs 
-  Copyright : (c) Jack Gang, 05/09/18 
+  Copyright : (c) Jack Gang, 05/15/18 
   Contains data types and image effect functions for PPM images.
 -}
 
@@ -17,6 +17,8 @@ module ImageEffects
    convolution
 ) where
 
+{----------------- DATA TYPES -----------------}
+
 data PPMImage a = PPMImage {width :: Integer, 
     height:: Integer,
     magicNumber :: Integer, 
@@ -26,28 +28,35 @@ data PPMImage a = PPMImage {width :: Integer,
 
 newtype Pixel a = Pixel (a,a,a) deriving (Show)
 
+instance Functor PPMImage where
+    fmap f (PPMImage w h mn mc p) = PPMImage w h mn mc (map f p)
+
+-- Kernel is just a list of Integers (assume length = 9)
 type Kernel = [Integer]
 
 newtype FixedPPMImage = FixedPPMImage {getImg:: PPMImage (Pixel Integer)} deriving (Show)
 
+-- mempty is a 10x10 black picture, since we want 0 values for our "Sum" function
 instance Monoid (FixedPPMImage) where
   mempty = FixedPPMImage (PPMImage 10 10 3 255 (replicate 100 (Pixel (0, 0, 0))))
   x `mappend` y = FixedPPMImage (PPMImage 10 10 3 255 (addPixelLists (pixels $ getImg x) (pixels $ getImg y)))
 
+{----------------- HELPER FUNCTIONS -----------------}
+
+-- constrains an Integer Pixel value between 0 and maxColor
 constrainPixelValue :: Integer -> Integer -> Integer
 constrainPixelValue val mc = if val > mc then mc
                              else if val < 0 then 0
                              else val
 
+-- Within a PPMImage, find a particular Pixel by its row and column indices
 findPixelByRC :: (Integral a) => PPMImage (Pixel a) -> Integer -> Integer -> Pixel a
 findPixelByRC ppm r c = let h = height ppm
                             w = width ppm
                         in if (r < 0) || (c < 0) || (r+1 > h) || (c+1 > w) then Pixel (0, 0, 0)
                            else (pixels ppm)!!(fromInteger (r*w + c))
 
-getValuesFromPixel :: Pixel a -> [a]
-getValuesFromPixel (Pixel (x, y, z)) = [x, y, z]
-
+-- get R, G, B values, respectively from a Pixel
 getR :: Pixel a -> a
 getR (Pixel (x, y, z)) = x
 
@@ -57,8 +66,11 @@ getG (Pixel (x, y, z)) = y
 getB :: Pixel a -> a
 getB (Pixel (x, y, z)) = z
 
+-- performs convolution on a given Pixel indicated by row and column indices within a PPMImage, using a given Kernel
+-- the "f" function parameter is either getR, getG, or getB to indicate which value is convoluted
 convolutionSingle :: PPMImage (Pixel Integer) -> Kernel -> Integer -> Integer -> (Pixel Integer -> Integer) -> Integer
 convolutionSingle ppm k r c f = let mc = maxColor ppm
+                              -- multiply 9 values in Kernel by 9 values around and including pixel
                               in constrainPixelValue (f (findPixelByRC ppm (r-1) (c-1)) * (k!!0) +
                                  f (findPixelByRC ppm (r-1) (c)) * (k!!1) +
                                  f (findPixelByRC ppm (r-1) (c+1)) * (k!!2) +
@@ -69,39 +81,40 @@ convolutionSingle ppm k r c f = let mc = maxColor ppm
                                  f (findPixelByRC ppm (r+1) (c)) * (k!!7) +
                                  f (findPixelByRC ppm (r+1) (c+1)) * (k!!8)) mc
 
-instance Functor PPMImage where
-    fmap f (PPMImage w h mn mc p) = PPMImage w h mn mc (map f p)
+-- sum the values of two lists of Pixels
+addPixelLists :: [Pixel Integer] -> [Pixel Integer] -> [Pixel Integer]
+addPixelLists [] [] = []
+addPixelLists (x:xs) (y:ys) = [Pixel (getR x + getR y, getG x + getG y, getB x + getB y)] ++ addPixelLists xs ys
 
+{----------------- IMAGE EFFECT FUNCTIONS -----------------}
+
+-- return the values of a Pixel in list format for output purposes
+getValuesFromPixel :: Pixel a -> [a]
+getValuesFromPixel (Pixel (x, y, z)) = [x, y, z]
+
+-- negate-red
 negateR :: (Integral a) => a -> Pixel a -> Pixel a
 negateR mc (Pixel (r, g, b)) = Pixel (mc-r, g, b)
 
+-- negate-green
 negateG :: (Integral a) => a -> Pixel a -> Pixel a
 negateG mc (Pixel (r, g, b)) = Pixel (r, mc-g, b)
 
+-- negate-blue
 negateB :: (Integral a) => a -> Pixel a -> Pixel a
 negateB mc (Pixel (r, g, b)) = Pixel (r, g, mc-b)
 
+-- grey-scale
 greyScale :: (Integral a) => Pixel a  -> Pixel a
 greyScale (Pixel (r, g, b)) = let avg = (r + g + b) `div` 3
                               in Pixel (avg, avg, avg)
 
+-- convolution given a PPMImage, a Kernel, and a starting row and column index (should be 0 and 0)
+-- convolutes each Pixel in an image, from left-to-right, top-to-bottom
 convolution :: PPMImage (Pixel Integer) -> Kernel -> Integer -> Integer -> [Pixel Integer]
 convolution ppm k startRow startCol = [Pixel (convolutionSingle ppm k startRow startCol getR,
                                              convolutionSingle ppm k startRow startCol getG,
                                              convolutionSingle ppm k startRow startCol getB)]
-                                      ++ if (startCol+1) >= (width ppm) && (startRow+1) >= (height ppm) then []
-                                         else if (startCol+1) >= (width ppm) then convolution ppm k (startRow+1) 0
-                                         else convolution ppm k startRow (startCol+1)
-
-addPixelLists :: [Pixel Integer] -> [Pixel Integer] -> [Pixel Integer]
-addPixelLists [] [] = []
---addPixelLists [] _ = []
-addPixelLists (x:xs) (y:ys) = [Pixel (getR x + getR y, getG x + getG y, getB x + getB y)] ++ addPixelLists xs ys
-
-sharpenKernel = [0, -1, 0, -1, 5, -1, 0, -1, 0]
-
-testImage1 = PPMImage 3 3 3 255 [(Pixel (1,1,1)), (Pixel (2,2,2)), (Pixel (3,3,3)),
-                                 (Pixel (4,4,4)), (Pixel (5,5,5)), (Pixel (6,6,6)),
-                                 (Pixel (7,7,7)), (Pixel (8,8,8)), (Pixel (9,9,9))]
-
-testImage2 = FixedPPMImage (PPMImage 10 10 3 255 (replicate 100 (Pixel (1, 1, 1))))
+                                      ++ if (startCol+1) >= (width ppm) && (startRow+1) >= (height ppm) then []     -- stop recursion when run out of pixels
+                                         else if (startCol+1) >= (width ppm) then convolution ppm k (startRow+1) 0  -- go to the next row
+                                         else convolution ppm k startRow (startCol+1)                               -- go to the next column
--- hw5/Ppm.hs
+++ hw5/Ppm.hs
@@ -1,14 +1,59 @@
+{- 
+  File      :  Ppm.hs 
+  Copyright : (c) Jack Gang, 05/15/18 
+  Contains the main IO functions for the PPM image editor.
+-}
+
 import Data.Char
 import ImageEffects
 
-setInputAndOutput :: IO (String, String)
-setInputAndOutput = do 
-    putStrLn "Enter in an input PPM file:"
-    inputStr <- getLine
-    putStrLn "Enter in an output PPM file:"
-    outputStr <- getLine
-    return (inputStr, outputStr) 
+{----------------- HELPER FUNCTIONS -----------------}
 
+-- check if an input file is valid before applying image effects
+checkIfInputValid :: [String] -> Bool
+checkIfInputValid header = if (header!!0 /= "P3") then False
+                           else if (isStrNumber (header!!1) == False || length (header!!1) == 0) then False
+                           else if (isStrNumber (header!!2) == False || length (header!!2) == 0) then False
+                           else True
+
+-- check if a String is numerical
+isStrNumber :: String -> Bool
+isStrNumber ""  = True
+isStrNumber (x:xs) = (isDigit x || x == ' ') && (isStrNumber xs)
+
+-- flatten list of Strings for easier input from .ppm file
+flattenList :: [String] -> [String]
+flattenList [] = []
+flattenList (x:xs) = words x ++ flattenList xs
+
+-- takes a list of String Pixel values and creates a list of Pixel Integers
+makeImageBodyList :: [String] -> [Pixel Integer]
+makeImageBodyList [] = []
+makeImageBodyList (x:y:z:xs) = [Pixel (read x :: Integer, read y :: Integer, read z :: Integer)] ++ makeImageBodyList xs
+
+-- takes a String List from input data and returns a PPM Image (see helper function above)
+getPPMFromData :: [String] -> PPMImage (Pixel Integer)
+getPPMFromData ppmData = let mn = toInteger (digitToInt (ppmData!!0!!1))
+                             w = read ((words (ppmData!!1))!!0) :: Integer
+                             h = read ((words (ppmData!!1))!!1) :: Integer
+                             mc = read (ppmData!!2) :: Integer
+                             (x:y:z:xs) = ppmData
+                             body = makeImageBodyList (flattenList xs)
+                         in PPMImage w h mn mc body
+
+-- returns a list of Integers from a list of Pixel Integers for output
+getOutputFromPixels :: [Pixel Integer] -> [Integer]
+getOutputFromPixels [] = []
+getOutputFromPixels (x:xs) = getValuesFromPixel x ++ getOutputFromPixels xs
+
+-- add new lines to each Integer in order to output in the correct format
+interspersePixelSpace :: [Integer] -> String
+interspersePixelSpace [] = []
+interspersePixelSpace (x:xs) = (show x) ++ "\n" ++ (interspersePixelSpace xs)
+
+{----------------- IO FUNCTIONS -----------------}
+
+-- initial menu for either blending or applying effects
 blendOrApply :: IO Int
 blendOrApply = do 
     putStrLn "============== Main Menu =============="
@@ -19,67 +64,16 @@ blendOrApply = do
     let optionInt = read optStr :: Int 
     return optionInt
 
-imageLoop :: PPMImage (Pixel Integer) -> String -> IO ()
-imageLoop ppm output = do
-    decision <- mainMenu
-    if decision == 1 then do
-        let newPPM = fmap (negateR (maxColor ppm)) $ ppm
-        putStrLn "Red-negated input PPM image."
-        imageLoop newPPM output
-    else if decision == 2 then do
-        let newPPM = fmap (negateG (maxColor ppm)) $ ppm
-        putStrLn "Green-negated input PPM image."
-        imageLoop newPPM output
-    else if decision == 3 then do
-        let newPPM = fmap (negateB (maxColor ppm)) $ ppm
-        putStrLn "Blue-negated input PPM image."
-        imageLoop newPPM output
-    else if decision == 4 then do
-        let newPPM = fmap greyScale $ ppm
-        putStrLn "Grey-scaled input PPM image."
-        imageLoop newPPM output
-    else if decision == 5 then do
-        let kernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1]
-            newPPM = PPMImage (width ppm) (height ppm) (magicNumber ppm) (maxColor ppm) (convolution ppm kernel 0 0)
-        putStrLn "Edge detection-ed input PPM image."
-        imageLoop newPPM output
-    else if decision == 6 then do
-        let kernel = [0, -1, 0, -1, 5, -1, 0, -1, 0]
-            newPPM = PPMImage (width ppm) (height ppm) (magicNumber ppm) (maxColor ppm) (convolution ppm kernel 0 0)
-        putStrLn "Sharpened input PPM image."
-        imageLoop newPPM output
-    else if decision == 7 then do
-        writeFile output $ ("P" ++ (show (magicNumber ppm)) ++ "\n" ++ 
-                            (show (width ppm)) ++ " " ++ (show (height ppm)) ++ "\n" ++
-                            (show (maxColor ppm)) ++ "\n" ++
-                            interspersePixelSpace (getOutputFromPixels (pixels ppm)))
-        putStrLn "Saved current PPM to output file."
-        imageLoop ppm output
-    else if decision == 8 then do
-        return ()
-    else do
-        putStrLn "Invalid choice. Please try again."
-        imageLoop ppm output
-
-blendLoop :: FixedPPMImage -> IO ()
-blendLoop initialFPPM = do
-    putStrLn "Enter in a Fixed PPM to blend:"
+-- input and output file for image effects
+setInputAndOutput :: IO (String, String)
+setInputAndOutput = do 
+    putStrLn "Enter in an input PPM file:"
     inputStr <- getLine
-    if inputStr == "q" then do
-        let ppm = getImg initialFPPM
-        writeFile "blended.ppm" $ ("P" ++ (show (magicNumber ppm)) ++ "\n" ++ 
-                                  (show (width ppm)) ++ " " ++ (show (height ppm)) ++ "\n" ++
-                                  (show (maxColor ppm)) ++ "\n" ++
-                                  interspersePixelSpace (getOutputFromPixels (pixels ppm)))
-        putStrLn "Saved current PPM to blended.ppm file."
-        main
-    else do
-        inputData <- readFile inputStr
-        let inputStrings = lines inputData
-        let ppm = getPPMFromData inputStrings
-        let fppm = FixedPPMImage (getImg $ (FixedPPMImage ppm) `mappend` initialFPPM)
-        blendLoop fppm
+    putStrLn "Enter in an output PPM file:"
+    outputStr <- getLine
+    return (inputStr, outputStr) 
 
+ -- main menu for image effect actions
 mainMenu :: IO Int  
 mainMenu = do 
     putStrLn "============== Apply Actions =============="
@@ -96,62 +90,84 @@ mainMenu = do
     let optionInt = read optStr :: Int 
     return optionInt
 
-checkIfInputValid :: [String] -> Bool
-checkIfInputValid header = if (header!!0 /= "P3") then False
-                           else if (isStrNumber (header!!1) == False || length (header!!1) == 0) then False
-                           else if (isStrNumber (header!!2) == False || length (header!!2) == 0) then False
-                           else True
+-- main loop for applying image effects
+imageLoop :: PPMImage (Pixel Integer) -> String -> IO ()
+imageLoop ppm output = do
+    decision <- mainMenu
+    if decision == 1 then do        -- negate-red
+        let newPPM = fmap (negateR (maxColor ppm)) $ ppm
+        putStrLn "Red-negated input PPM image."
+        imageLoop newPPM output
+    else if decision == 2 then do   -- negate-green
+        let newPPM = fmap (negateG (maxColor ppm)) $ ppm
+        putStrLn "Green-negated input PPM image."
+        imageLoop newPPM output
+    else if decision == 3 then do   -- negate-blue
+        let newPPM = fmap (negateB (maxColor ppm)) $ ppm
+        putStrLn "Blue-negated input PPM image."
+        imageLoop newPPM output
+    else if decision == 4 then do   -- grey-scale
+        let newPPM = fmap greyScale $ ppm
+        putStrLn "Grey-scaled input PPM image."
+        imageLoop newPPM output
+    else if decision == 5 then do   -- edge detection
+        let kernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1]    -- edge detection Kernel
+            newPPM = PPMImage (width ppm) (height ppm) (magicNumber ppm) (maxColor ppm) (convolution ppm kernel 0 0)
+        putStrLn "Edge detection-ed input PPM image."
+        imageLoop newPPM output
+    else if decision == 6 then do   -- sharpen
+        let kernel = [0, -1, 0, -1, 5, -1, 0, -1, 0]        -- sharpen Kernel
+            newPPM = PPMImage (width ppm) (height ppm) (magicNumber ppm) (maxColor ppm) (convolution ppm kernel 0 0)
+        putStrLn "Sharpened input PPM image."
+        imageLoop newPPM output
+    else if decision == 7 then do   -- save to output
+        writeFile output $ ("P" ++ (show (magicNumber ppm)) ++ "\n" ++ 
+                            (show (width ppm)) ++ " " ++ (show (height ppm)) ++ "\n" ++
+                            (show (maxColor ppm)) ++ "\n" ++
+                            interspersePixelSpace (getOutputFromPixels (pixels ppm)))
+        putStrLn "Saved current PPM to output file."
+        imageLoop ppm output
+    else if decision == 8 then do   -- exit
+        return ()
+    else do
+        putStrLn "Invalid choice. Please try again."
+        imageLoop ppm output
 
-isStrNumber :: String -> Bool
-isStrNumber ""  = True
-isStrNumber (x:xs) = (isDigit x || x == ' ') && (isStrNumber xs)
-
--- pixelLineToPixels :: [String] -> [Pixel Integer]
--- pixelLineToPixels [] = []
--- pixelLineToPixels [x] = []
--- pixelLineToPixels [x, _] = []
--- pixelLineToPixels (x:y:z:xs) = [Pixel (read x :: Integer, read y :: Integer, read z :: Integer)] ++ pixelLineToPixels xs
-
-flattenList :: [String] -> [String]
-flattenList [] = []
-flattenList (x:xs) = words x ++ flattenList xs
-
-makeImageBodyList :: [String] -> [Pixel Integer]
-makeImageBodyList [] = []
-makeImageBodyList (x:y:z:xs) = [Pixel (read x :: Integer, read y :: Integer, read z :: Integer)] ++ makeImageBodyList xs
-
-getPPMFromData :: [String] -> PPMImage (Pixel Integer)
-getPPMFromData ppmData = let mn = toInteger (digitToInt (ppmData!!0!!1))
-                             w = read ((words (ppmData!!1))!!0) :: Integer
-                             h = read ((words (ppmData!!1))!!1) :: Integer
-                             mc = read (ppmData!!2) :: Integer
-                             (x:y:z:xs) = ppmData
-                             body = makeImageBodyList (flattenList xs)
-                         in PPMImage w h mn mc body
-
-getOutputFromPixels :: [Pixel Integer] -> [Integer]
-getOutputFromPixels [] = []
-getOutputFromPixels (x:xs) = getValuesFromPixel x ++ getOutputFromPixels xs
-
-interspersePixelSpace :: [Integer] -> String
-interspersePixelSpace [] = []
-interspersePixelSpace (x:xs) = (show x) ++ " \n" ++ (interspersePixelSpace xs)
+-- main loop for blending images
+blendLoop :: FixedPPMImage -> IO ()
+blendLoop initialFPPM = do
+    putStrLn "Enter in a Fixed PPM to blend:"
+    inputStr <- getLine
+    if inputStr == "q" then do          -- save file to blended.ppm and quit to main menu
+        let ppm = getImg initialFPPM
+        writeFile "blended.ppm" $ ("P" ++ (show (magicNumber ppm)) ++ "\n" ++ 
+                                  (show (width ppm)) ++ " " ++ (show (height ppm)) ++ "\n" ++
+                                  (show (maxColor ppm)) ++ "\n" ++
+                                  interspersePixelSpace (getOutputFromPixels (pixels ppm)))
+        putStrLn "Saved current PPM to blended.ppm file."
+        main
+    else do                             -- blend with previous ppm and prompt further input
+        inputData <- readFile inputStr
+        let inputStrings = lines inputData
+        let ppm = getPPMFromData inputStrings
+        let fppm = FixedPPMImage (getImg $ (FixedPPMImage ppm) `mappend` initialFPPM)
+        blendLoop fppm
 
 main :: IO () 
 main = do
     mainDecision <- blendOrApply
-    if mainDecision == 1 then do
+    if mainDecision == 1 then do        -- apply image effects
         files <- setInputAndOutput
         inputData <- readFile (fst files)
         let inputStrings = lines inputData
         if checkIfInputValid inputStrings == False then do
             putStrLn "Invalid Input File"
             return ()
-        else do
+        else do                         
             let outputFile = snd files
             let ppm = getPPMFromData inputStrings
             imageLoop ppm outputFile
-    else if mainDecision == 2 then do
+    else if mainDecision == 2 then do   -- blend
         let fppm = FixedPPMImage (PPMImage 10 10 3 255 (replicate 100 (Pixel (0, 0, 0))))
         blendLoop fppm
     else do
