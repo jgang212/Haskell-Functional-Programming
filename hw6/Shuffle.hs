66eda510135d0fa3315e7fac8bba78c66e85d801
Jack Gang <jack.gang@xrtrading.com>
2018-05-30 12:53:16 -0500

    add shuffle class

---
+++ hw6/Shuffle.hs
@@ -0,0 +1,29 @@
+{- 
+  File      :  Shuffle.hs 
+  Unchanged module given by instructor.
+-}
+
+module Shuffle
+(
+  shuffle, 
+)
+where 
+
+import System.Random
+import Data.Array.IO
+import Control.Monad
+ 
+-- Randomly shuffle a list
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs
